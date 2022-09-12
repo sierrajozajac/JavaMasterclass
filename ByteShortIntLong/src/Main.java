a2520a9b2faaff210ea1549455a904a97ddc0892
Sierra Jo Sallee <31775439+sierrasallee@users.noreply.github.com>
2022-09-11 19:29:39 -0700

    Update Main.java

--- ByteShortIntLong/src/Main.java
+++ ByteShortIntLong/src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        //intTest();
-        //byteTest();
-        //shortTest();
+        intTest();
+        byteTest();
+        shortTest();
         longTest();
+        primitiveTypesChallenge();
     }
 
     public static void intTest() {
@@ -19,8 +20,13 @@ public class Main {
         // Now some underflow!
         System.out.println("Busted minimum value = " + (myMinIntValue - 1));
 
+        // IntelliJ knows the literal limits to ints
         //int myMaxIntTest = 2147483648;
         //int myMinIntTest = -2147483649;
+
+        // Casting isn't necessary with arithmetic expressions
+        int total1 = myValue / 2;
+        int total2 = (int) (myMinIntValue / 2);
     }
 
     public static void byteTest() {
@@ -31,6 +37,10 @@ public class Main {
         System.out.println("Byte Max Value: " + myMaxByteValue);
         System.out.println("Byte Minimum Value - 1: " + (myMinByteValue - 1));
         System.out.println("Byte Max Value + 1: " + (myMaxByteValue + 1));
+
+        // Casting is necessary with arithmetic expressions
+        byte total1 = (byte) (myMinByteValue / 2);
+        byte total2 = (byte) (myMaxByteValue / 2);
     }
 
     public static void shortTest() {
@@ -41,6 +51,10 @@ public class Main {
         System.out.println("Short Max Value: " + myMaxShortValue);
         System.out.println("Short Minimum Value - 1: " + (myMinShortValue - 1));
         System.out.println("Short Max Value + 1: " + (myMaxShortValue + 1));
+
+        // Casting is necessary with arithmetic expressions
+        short total1 = (short) (myMinShortValue / 2);
+        short total2 = (short) (myMaxShortValue / 2);
     }
 
     public static void longTest() {
@@ -52,5 +66,17 @@ public class Main {
         System.out.println("Long Max Value: " + myMaxLongValue);
         System.out.println("Busted Minimum Value: " + (myMinLongValue - 1));
         System.out.println("Busted Max Value: " + (myMaxLongValue + 1));
+
+        // Casting isn't necessary with arithmetic expressions
+        long total1 =  myLongValue / 2;
+        long total2 = (long) (bigLongLiteralValue / 2);
+    }
+
+    public static void primitiveTypesChallenge() {
+        byte b_value = 45;
+        short s_value = 6787;
+        int i_value = 245893;
+        long total = 50_000L + (10L * (b_value + s_value + i_value));
+        System.out.println("Total: " + total);
     }
 }
